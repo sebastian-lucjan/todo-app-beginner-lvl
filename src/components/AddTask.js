13a08ce89cbc28195a76422c90b2f9b9f874af61
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-03-30 08:51:18 +0200

    AddTask.js: add JSX and handle methods

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
+import './AddTask.css';
 
 class AddTask extends Component {
-  state = {};
+  minDate = new Date().toISOString().slice(0, 10);
+
+  state = {
+    text: '',
+    checked: false,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  // handleText = (e) => {
+  //   this.setState({
+  //     text: e.target.value,
+  //   });
+  // };
+  // handleCheckbox = (e) => {
+  //   this.setState({
+  //     text: e.target.checked,
+  //   });
+  // };
+
+  handleText = (e) => {
+    this.setState({ text: e.target.value });
+    console.log(this.state.text);
+  };
+
+  handleChange = (e, type) => {
+    if ((type = 'checkbox')) this.setState({ date: e.target.checked });
+    else this.setState({ date: e.target.value });
+  };
+
   render() {
+    const maxDate = this.minDate.slice(0, 4) * 1 + 2 + '-12-31';
+    console.log(maxDate);
+
     return (
       <>
-        <div>
-          <h3>Dodaj zadanie</h3>
+        <div className="form">
+          <input
+            type="text"
+            placeholder="dodaj zadanie..."
+            value={this.state.text}
+            onChange={this.handleText}
+          />
+          <input
+            type="checkbox"
+            id="important"
+            onChange={(e) => this.handleChange(e, 'checkbox')}
+          />
+          <label htmlFor="important">Priorytet</label>
+          <br />
+          <label htmlFor="date">Do kiedy zrobić</label>
+          <input
+            type="date"
+            value={this.state.date}
+            min={this.minDate}
+            max={maxDate}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button>Dodaj</button>
         </div>
       </>
     );
